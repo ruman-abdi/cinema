104d9d0f39053abaa956bb55a09cd01e1fa53ad2
ruman-abdi <rahrumanabdii@gmail.com>
2023-05-02 19:50:01 -0700

    remove file Revert "message"
    This reverts commit a12641f4783a56548005c548eddb915bd481a665.

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -18,16 +18,6 @@ const MovieCard = (props) => {
      <div>
      <span>{Type}</span>
      <h3>{Title}</h3>
-     <p>{Year}</p>
-     </div>
-  <div>
-  <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
-      
-  </div>
-
-  <div>
-  <span>{Type}</span>
-  <h3>{Title}</h3>
      </div>
     </div>
     </div>
